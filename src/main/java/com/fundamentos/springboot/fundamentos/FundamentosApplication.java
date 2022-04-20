fd201fada3f4a78aa74f3de00722f40c07cda087
Iván Portillo Perez <ivan.portillo@unir.net>
2022-04-20 08:49:58 +0200

    Agragado fichero pojo y properties

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,11 +1,11 @@
 package com.fundamentos.springboot.fundamentos;
 
-import com.fundamentos.springboot.fundamentos.bean.LoginInterface;
-import com.fundamentos.springboot.fundamentos.bean.MyBean;
-import com.fundamentos.springboot.fundamentos.bean.MyBeanImplement;
-import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.*;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,18 +14,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
-
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private LoginInterface loginInterface;
+	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, LoginInterface loginInterface) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, LoginInterface loginInterface, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.loginInterface = loginInterface;
+		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -34,11 +38,22 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		componentDependency.saludar();
-		myBean.print();
-		myBeanWithDependency.printWithDependency();
-		loginInterface.printWelcome();
+		try{
+			componentDependency.saludar();
+			myBean.print();
+			myBeanWithDependency.printWithDependency();
+			loginInterface.printWelcome();
+			System.out.println(myBeanWithProperties.function());
+			System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
 
+			//generar error
+			int value = 10/0;
+			LOGGER.info("Mi valor es: " + value);
+
+		}catch (Exception exception) {
+			LOGGER.error("Esto es un error del aplicativo: " + exception.getMessage());
+
+		}
 
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDpendencyImplements.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDpendencyImplements.java
@@ -1,6 +1,11 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class MyBeanWithDpendencyImplements  implements MyBeanWithDependency{
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDpendencyImplements.class);
 
     private MyOperation myOperation;
 
@@ -10,7 +15,10 @@ public class MyBeanWithDpendencyImplements  implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
-        System.out.println(myOperation.sum(1));
+        LOGGER.info("Hemos ingresado al método printWithDependency de la clase MyBeanWithDpendencyImplements");
+        int numero = 2;
+        LOGGER.debug("El número enviado por parametro a la dependencia operación es: " + numero);
+        System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementación de un bean con dependencia");
     }
 }
